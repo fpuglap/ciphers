67a01191dcca41644f197bb4b3b9c02725f314d1
Facundo Pugliese <fpugliese09@gmail.com>
2023-05-05 01:58:06 -0300

    Minor changes

--- app/cipher.tsx
+++ app/cipher.tsx
@@ -16,78 +16,76 @@ const Cipher = () => {
   };
 
   return (
-    <div className='flex z-10 w-full max-w-6xl justify-center items-center mt-10 mb-96'>
-      <div className='flex flex-col w-full justify-center items-center'>
-        <h1 className='left-0 flex w-full text-4xl justify-center from-zinc-200 pb-2 dark:from-inherit lg:static lg:w-auto lg:rounded-xl'>
-          <code className='font-mono font-bold'>Caesar Shift Cipher</code>
-        </h1>
-        <article className='mx-8'>
-          <section className='indent-8 my-3 text-left w-full text-lg lg:max-w-4xl'>
-            Cryptography is the science (and art) of writing messages that no
-            one but the intended receiver can read. The name derives from either
-            the French word cryptographie or from Modern Latin cryptographia
-            both of which find their roots in the Greek <em>kryptos</em>{' '}
-            “hidden” + <em>graphia</em> “writing or recording”.
-          </section>
-          <section className='my-3 w-full text-left text-lg from-zinc-200 pb-2 dark:from-inherit lg:max-w-4xl lg:static lg:w-auto lg:rounded-xl mx-auto'>
-            Historically, the human race needed to communicate and share
-            information with others, but it also needed to communicate
-            selectively. That&apos;s why this “art” started to be performed,
-            hiding messages from unauthorized people until the message reaches
-            the intended hand. The first evidence of cryptography is the use of
-            hieroglyphs by the Egyptians.
-          </section>
-          <section className='my-3 w-full text-left text-lg from-zinc-200 pb-2 dark:from-inherit lg:max-w-4xl lg:static lg:w-auto lg:rounded-xl mx-auto'>
-            Later on the Romans popularized the cryptography method known as the{' '}
-            <strong>Caesar Shift Cipher</strong> which relies on shifting the
-            letters of a message by an agreed number. Then, the receiver of the
-            message would shift the letters by the same number again to obtain
-            the original message.
-          </section>
-          <section className='my-3 w-full text-left text-lg from-zinc-200 pb-2 dark:from-inherit lg:max-w-4xl lg:static lg:w-auto lg:rounded-xl mx-auto'>
-            Here is a quick example: <br />
-            <em>Gmfyjcjmjuz up xpsl gspn ipnf ps boz puifs qmbdf</em>
-            <br />
-            <br />
-            To cipher this phrase using Caesar method one would shift each
-            letter up in the alphabet. <br />
-            plain: <em>abcdefghijklmnopqrstuvwxyz</em>
-            <br />
-            cipher: <em>bcdefghijklmnopqrstuvwxyza</em>
-            <br />
-            <br />
-            In this case, G becomes F, m becomes l, f becomes e… and so on until
-            you get the full phrase: Flexibility to work from home or any other
-            place.
-          </section>
-        </article>
-        <input
-          className='flex justify-center items-center w-full h-20 text-4xl px-5 py-2 mt-5 rounded-md border focus:outline-none focus:ring focus:border-[#0141ff]  border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-600 dark:bg-opacity-30 leading-tight lg:w-8/12'
-          type='text'
-          placeholder='Give it a try...'
-          onChange={handleChange}
-        />
-        {decryptedMessage && (
-          <>
-            <p className='fixed left-0 flex w-full mt-20 text-4xl justify-center from-zinc-200 pb-3 dark:from-inherit lg:static lg:w-auto lg:rounded-xl'>
-              <code className='font-mono'>Decrypted message</code>
-            </p>
-            <p className='fixed left-0 flex w-full mx-10 text-4xl justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-              {decryptedMessage}
-            </p>
-          </>
-        )}
-        {encryptedMessage && (
-          <>
-            <p className='fixed left-0 flex w-full mt-20 text-4xl justify-center from-zinc-200 pb-3 dark:from-inherit lg:static lg:w-auto lg:rounded-xl'>
-              <code className='font-mono'>Encrypted message</code>
-            </p>
-            <p className='fixed left-0 flex w-full mx-10 text-4xl justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-              {encryptedMessage}
-            </p>
-          </>
-        )}
-      </div>
+    <div className='flex flex-col w-full justify-center items-center'>
+      <h1 className='left-0 flex w-full text-4xl justify-center from-zinc-200 pb-2 dark:from-inherit lg:static lg:w-auto lg:rounded-xl'>
+        <code className='font-mono font-bold'>Caesar Shift Cipher</code>
+      </h1>
+      <article className='mx-8'>
+        <section className='indent-8 my-3 text-left w-full text-lg lg:max-w-4xl'>
+          Cryptography is the science (and art) of writing messages that no one
+          but the intended receiver can read. The name derives from either the
+          French word cryptographie or from Modern Latin cryptographia both of
+          which find their roots in the Greek <em>kryptos</em> “hidden” +{' '}
+          <em>graphia</em> “writing or recording”.
+        </section>
+        <section className='my-3 w-full text-left text-lg from-zinc-200 pb-2 dark:from-inherit lg:max-w-4xl lg:static lg:w-auto lg:rounded-xl mx-auto'>
+          Historically, the human race needed to communicate and share
+          information with others, but it also needed to communicate
+          selectively. That&apos;s why this “art” started to be performed,
+          hiding messages from unauthorized people until the message reaches the
+          intended hand. The first evidence of cryptography is the use of
+          hieroglyphs by the Egyptians.
+        </section>
+        <section className='my-3 w-full text-left text-lg from-zinc-200 pb-2 dark:from-inherit lg:max-w-4xl lg:static lg:w-auto lg:rounded-xl mx-auto'>
+          Later on the Romans popularized the cryptography method known as the{' '}
+          <strong>Caesar Shift Cipher</strong> which relies on shifting the
+          letters of a message by an agreed number. Then, the receiver of the
+          message would shift the letters by the same number again to obtain the
+          original message.
+        </section>
+        <section className='my-3 w-full text-left text-lg from-zinc-200 pb-2 dark:from-inherit lg:max-w-4xl lg:static lg:w-auto lg:rounded-xl mx-auto'>
+          Here is a quick example: <br />
+          <em>Gmfyjcjmjuz up xpsl gspn ipnf ps boz puifs qmbdf</em>
+          <br />
+          <br />
+          To cipher this phrase using Caesar method one would shift each letter
+          up in the alphabet. <br />
+          plain: <em>abcdefghijklmnopqrstuvwxyz</em>
+          <br />
+          cipher: <em>bcdefghijklmnopqrstuvwxyza</em>
+          <br />
+          <br />
+          In this case, G becomes F, m becomes l, f becomes e… and so on until
+          you get the full phrase: Flexibility to work from home or any other
+          place.
+        </section>
+      </article>
+      <input
+        className='flex justify-center items-center w-full h-20 text-4xl px-5 py-2 mt-5 rounded-md border focus:outline-none focus:ring focus:border-[#0141ff]  border-gray-300 bg-gray-100 dark:border-neutral-700 dark:bg-neutral-600 dark:bg-opacity-30 leading-tight lg:w-8/12'
+        type='text'
+        placeholder='Give it a try...'
+        onChange={handleChange}
+      />
+      {decryptedMessage && (
+        <>
+          <p className='fixed left-0 flex w-full mt-20 text-4xl justify-center from-zinc-200 pb-3 dark:from-inherit lg:static lg:w-auto lg:rounded-xl'>
+            <code className='font-mono'>Decrypted message</code>
+          </p>
+          <p className='fixed left-0 flex w-full mx-10 text-4xl justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+            {decryptedMessage}
+          </p>
+        </>
+      )}
+      {encryptedMessage && (
+        <>
+          <p className='fixed left-0 flex w-full mt-20 text-4xl justify-center from-zinc-200 pb-3 dark:from-inherit lg:static lg:w-auto lg:rounded-xl'>
+            <code className='font-mono'>Encrypted message</code>
+          </p>
+          <p className='fixed left-0 flex w-full mx-10 text-4xl justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
+            {encryptedMessage}
+          </p>
+        </>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,19 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className='flex flex-col items-center justify-between p-24'>
-      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono lg:flex'>
         <p className='fixed left-0 top-0 text-lg flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
           Get started with&nbsp;
           <code className='font-mono font-bold'>ciphers</code>
         </p>
-        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
+        <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto lg:bg-none'>
           <Link
-            className='pointer-events-none text-lg flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
+            className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
             href='/'
           >
-            By{' '}
+            <p className='invisible hidden lg:visible lg:block lg:text-lg'>
+              By{' '}
+            </p>
             <Image
               priority
               sizes='4rem'
@@ -31,7 +33,11 @@ export default function Home() {
         </div>
       </div>
       <div className="relative flex place-items-center before:absolute before:h-[700px] before:w-[880px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"></div>
-      <Cipher />
+      <div>Este es un div</div>
+
+      {/* <div className='flex z-10 w-full max-w-6xl justify-center items-center mt-10 mb-96'>
+        <Cipher />
+      </div> */}
       <div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'></div>
     </main>
   );
