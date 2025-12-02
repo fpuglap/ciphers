a31e62d466e196e747120b6d17eaaff2a7e26b25
Facundo Pugliese <fpugliese09@gmail.com>
2025-12-02 18:35:38 -0300

    Add OpenGraph metadata and social preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
 import './globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
+  metadataBase: new URL('https://caesar-shift-cipher.vercel.app'),
   title: 'Caesar Shift Cipher',
   description:
     'The Caesar Shift Cipher relies on shifting the letters of a message by an agreed number.',
+  openGraph: {
+    title: 'Caesar Shift Cipher',
+    description:
+      'The Caesar Shift Cipher relies on shifting the letters of a message by an agreed number.',
+    url: 'https://caesar-shift-cipher.vercel.app',
+    images: ['/caesar-og.png'],
+  },
 };
 
 export default function RootLayout({
@@ -13,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
